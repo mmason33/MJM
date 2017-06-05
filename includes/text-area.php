2e309449c30d769edca585125b939728bc07aeef
michael mason <michael@michaeljmason.com>
2017-06-05 16:19:48 -0700

    setting up the nav and section id's

--- includes/text-area.php
+++ includes/text-area.php
@@ -1,19 +1,23 @@
 
 
 <?php if( have_rows('text_box')): ?>
-	<div class="slider-pro" id="my-slider">
+	<div class="slider-pro" id="my-slider" data-aos="zoom-in" data-aos-once="true">
 		<div class="sp-slides">
+<?php $i = 1; ?>
 	<?php while ( have_rows('text_box') ) : the_row(); ?>
 
 			<div class="sp-slide">
-				<div class="text-center slide-wrap">
+				<div id="<?php echo $i; ?>" class="text-center slide-wrap">
 					<?php echo get_sub_field('text_area'); ?>
 				</div>
 			</div>
-
+	<?php $i++; ?>
 	<?php endwhile; ?>
 		</div>
 	</div>
+
+<?php else: ?>
+	
 <?php endif; ?>
 
 
--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,10 @@
-<!-- <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?//= esc_url(home_url('/')); ?>"><?php //bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      //if (has_nav_menu('primary_navigation')) :
-        //wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      //endif;
-      ?>
-    </nav>
+<header class="navigation" data-aos="slide-down" data-aos-once="true">
+  <div class="nav-wrap">
+    <ul class="nav-list">
+      <li><a class="active-item" href="/#1">Home</a></li>
+      <li><a href="/#2">About</a></li>
+      <li><a href="/#3">Process</a></li>
+      <li><a href="/#4">Contact</a></li>
+    </ul>
   </div>
-</header> -->
+</header>
